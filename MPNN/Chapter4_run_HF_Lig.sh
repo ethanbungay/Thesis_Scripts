91f1b7115e18c6640ee435ca02e4fc06bd8d5051
ethanbungay <ethan.bungay@bristol.ac.uk>
2026-04-14 10:25:23 +0100

    Update and rename run_HFs_Lig.sh to Chapter4_run_HF_Lig.sh

--- MPNN/Chapter4_run_HF_Lig.sh
+++ MPNN/Chapter4_run_HF_Lig.sh
@@ -6,13 +6,13 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=00:05:00
 #SBATCH --mem=10GB
-#SBATCH --account=chem023222
+#SBATCH --account=xxxx
 
 
-/user/work/dy19627/Anaconda3/envs/ligandmpnn_env2/bin/python3.11 run.py \
+run.py \
 	--model_type "ligand_mpnn" \
-	--pdb_path "./inputs/HF/HF_v2s2_ID214.pdb" \
-	--out_folder "./outputs/HF_ShortMaquette/Attempt_2/L2_v2_ID214input" \
+	--pdb_path "./inputs/HF/m2-4D2_flav.pdb" \
+	--out_folder "./outputs/HF/S1L1" \
 	--redesigned_residues "A6 A10 A13 A16 A19 A20 A23 A24 A26 A29 A30 A31 A33 A34 A36 A38 A40 A41 A42 A44 A45 A48 A49 A51 A52 A56 A64 A68 A71 A74 A78 A81 A82 A87 A92 A95 A99 A102 A103 A106 A107 A109 A110" \
 	--omit_AA "HCWM" \
 	--batch_size 250
