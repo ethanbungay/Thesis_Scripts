80e197c3b895640e45cff224ba1a8ae5d9ed684a
ethanbungay <ethan.bungay@bristol.ac.uk>
2026-04-14 10:37:18 +0100

    Update and rename v2_HF_FMN_lig.sh to Chapter5_Classic_HF_FMN_lig.sh

--- MPNN/Chapter5_Classic_HF_FMN_lig.sh
+++ MPNN/Chapter5_Classic_HF_FMN_lig.sh
@@ -9,10 +9,10 @@
 #SBATCH --account=chem023222
 
 
-/user/work/dy19627/Anaconda3/envs/ligandmpnn_env2/bin/python3.11 run.py \
+run.py \
 	--model_type "ligand_mpnn" \
-	--pdb_path "./inputs/HF_NonCov/v2/S3L3_HF_FMN_id76model1_NoHeme.pdb" \
-	--out_folder "./outputs/NonCov_ID76_FlavinOnly/v3_lig" \
+	--pdb_path "./inputs/m2-4D2_FMN.pdb" \
+	--out_folder "./outputs/HF_FMN_Classic/" \
 	--redesigned_residues "A6 A10 A13 A16 A19 A20 A23 A24 A26 A29 A30 A31 A33 A34 A36 A37 A38 A40 A41 A42 A44 A45 A48 A49 A51 A52 A56 A64 A68 A71 A74 A81 A82 A87 A92 A95 A99 A102 A103 A106 A107 A109 A110" \
 	--omit_AA "HC" \
 	--batch_size 250
