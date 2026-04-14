c5dc04abfd847a889eacf881b96c1e1fd5e280fe
ethanbungay <ethan.bungay@bristol.ac.uk>
2026-04-14 10:29:49 +0100

    Update run_PHF_v5_ligandmpnn.sh

--- MPNN/run_PHF_v5_ligandmpnn.sh
+++ MPNN/run_PHF_v5_ligandmpnn.sh
@@ -6,15 +6,14 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=00:15:00
 #SBATCH --mem=50GB
-#SBATCH --account=chem023222
+#SBATCH --account=xxxx
 
 
-/user/work/dy19627/Anaconda3/envs/ligandmpnn_env2/bin/python3.11 run.py \
+run.py \
 	--model_type "ligand_mpnn" \
-	--checkpoint_path_sc "./model_params/ligandmpnn_sc_v_32_002_16.pt" \
-	--pdb_path "./inputs/PHF_4D2pt5/PHF_XF_v5pt3_biases_pt3_ID145.pdb" \
-	--out_folder "./outputs/PHF_4D2pt5/XstalFlavin_v5pt3_biases_pt4" \
+	--pdb_path "./inputs/PHF_4D2pt5.pdb" \
+	--out_folder "./outputs/PHF/S0L1" \
 	--bias_AA_per_residue "./inputs/PHF_4D2pt5/PHF_4D2pt5_v5_XstalFlavOxyRenumb_biaspt4.json" \
-	--fixed_residues "A1 A16 A44 A54 A88 A126" \
+	--fixed_residues "A17 A20 A23 A26 A27 A30 A31 A33 A35 A36 A37 A38 A39 A40 A41 A43 A45 A48 A51 A52 A55 A56 A58 A59 A62 A81 A85 A89 A92 A95 A99 A102 A103 A108 A109 A112 A113 A116 A120 A123 A124 A127 A128 A130 A131" \
 	--omit_AA "HC" \
 	--batch_size 250
