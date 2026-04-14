3a9efd50b238974efd21a0ae6211c49c31193cfe
ethanbungay <ethan.bungay@bristol.ac.uk>
2026-04-14 10:36:26 +0100

    Update and rename v2_HF_FMN_Sol.sh to Chapter5_Classic_HF_FMN_Sol.sh

--- MPNN/Chapter5_Classic_HF_FMN_Sol.sh
+++ MPNN/Chapter5_Classic_HF_FMN_Sol.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #SBATCH --partition=veryshort
 #SBATCH --nodes=1
-#SBATCH --job-name=LigMPNN
+#SBATCH --job-name=SolMPNN
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=00:15:00
 #SBATCH --mem=50GB
-#SBATCH --account=chem023222
+#SBATCH --account=xxxx
 
-
-/user/work/dy19627/Anaconda3/envs/ligandmpnn_env2/bin/python3.11 run.py \
+run.py \
 	--model_type "soluble_mpnn" \
-	--pdb_path "./inputs/HF_NonCov/v2_WFix/S1L1_HF_FMN_id231.pdb" \
-	--out_folder "./outputs/HF_FMN_v2/S2L1_W78Fixed/" \
+	--pdb_path "./inputs/m2-4D2_FMN.pdb" \
+	--out_folder "./outputs/HF_FMN_Classic/" \
 	--fixed_residues "A1 A6 A9 A10 A13 A16 A19 A20 A23 A24 A26 A29 A30 A31 A33 A34 A36 A37 A38 A40 A41 A42 A44 A45 A48 A49 A51 A52 A56 A64 A67 A68 A71 A74 A78 A81 A82 A87 A92 A95 A99 A102 A103 A106 A107 A109 A110" \
 	--omit_AA "HC" \
 	--batch_size 250
